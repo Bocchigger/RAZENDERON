0e6bed8f09b641119e1e5772259205c1e78344b7
DreamingVine <dreamingvine@gmail.com>
2026-02-10 13:44:55 +0100

    Better 404 page with redirect to home

--- app/404.php
+++ app/404.php
@@ -1,3 +1,5 @@
 <?php
 
-echo("WRONG LINK HAHA");
+echo("The page you are looking for does not exist. Please check the URL and try again.");
+echo("<br>");
+echo("Go back to the website <a href='/'>here</a>");
